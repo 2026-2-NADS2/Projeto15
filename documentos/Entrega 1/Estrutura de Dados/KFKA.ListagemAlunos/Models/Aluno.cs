017b11f6c75fcc7617ec681229490fb54c489792
rntoscano <anny.toscano@hotmail.com.com>
2026-09-25 23:37:08 -0300

    estrutura de dados23

--- documentos/Entrega 1/Estrutura de Dados/KFKA.ListagemAlunos/Models/Aluno.cs
+++
@@ -1,9 +0,0 @@
-namespace KFKA.ListagemAlunos.Models;
-
-public class Aluno
-{
-    public int Id { get; set; }
-    public string Nome { get; set; } = string.Empty;
-    public string Matricula { get; set; } = string.Empty;
-    public string Turma { get; set; } = string.Empty;
-}
